ac4a7edad7677c7db85a7fe09ddc2b01a4367d50
somadattareddy <dattareddy1911@gmail.com>
2024-05-09 11:19:23 +0530

    all features done, styling yet to be done

--- image_handler/urls.py
+++ image_handler/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('upload/', views.upload_image,name='upload_image'),
     path('assign-label/<int:pk>/', views.assign_label,name='assign_label'),
     path('label-assigned/', views.label_assigned, name='label_assigned'),
+    path('edit_label/<int:pk>/', views.edit_label, name='edit_label'),
     path('view-uploaded-images/', views.view_uploaded_images, name='view_uploaded_images'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('accounts/login/', views.login_view, name='login'),
